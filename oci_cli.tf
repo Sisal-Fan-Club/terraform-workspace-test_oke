116339c4e972328a5317bde4b18f87d2280de62a
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 19:21:50 +0200

    Update oci_cli.tf

--- oci_cli.tf
+++ oci_cli.tf
@@ -40,3 +40,21 @@ resource "kubernetes_secret" "terraform_cloud" {
   }
   type = "kubernetes.io/service-account-token"
 }
+
+resource "kubernetes_cluster_role_binding" "terraform_cloud" {
+  metadata {
+    name = "terraform_cloud"
+  }
+  
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+  
+  subject {
+    kind = "ServiceAccount"
+    name = kubernetes_service_account.terraform_cloud.metadata[0].name
+    namespace = kubernetes_service_account.terraform_cloud.metadata[0].namespace
+  }
+}
