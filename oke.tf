70c02a3803bd2a90da4d5a97ea7f949f769f22ce
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 16:28:46 +0200

    Create oke.tf

---
+++ oke.tf
@@ -0,0 +1,30 @@
+locals {
+  test_compartment = local.tfe_workspace_outputs.test_compartment.test_compartment
+  
+  vcn = local.tfe_workspace_outputs.vcn.vcn
+  app_subnet = local.tfe_workspace_outputs.test_subnets.app_subnet
+  dmz_subnet = local.tfe_workspace_outputs.test_subnets.dmz_subnet
+  
+  test_oke = oci_containerengine_cluster.test_oke
+}
+
+resource "oci_containerengine_cluster" "test_oke" {
+  compartment_id = local.test_compartment.id
+  vcn_id = local.vcn.id
+  
+  name = "Sisal Fan Club - Cluster OKE - Test Environment"
+  kubernetes_version = "v1.24.1"
+  
+  endpoint_config {
+    subnet_id = local.dmz_subnet.id
+    is_public_ip_enabled = true
+  }
+  
+  service_lb_subnet_ids = [
+    local.dmz_subnet.id
+  ]
+  service_lb_config {
+    freeform_tags = merge({
+    }, local.dmz_subnet.freeform_tags, local.vcn.freeform_tags, local.test_compartment.freeform_tags)
+  }
+}
